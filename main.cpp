309b7c660836fcdf947ef6af46d65af6820d061e
PANKAJMOR <morepankaj2004@gmail.com>
2023-10-27 10:00:19 +0530

    delete

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-cout<<"hello world"<<endl;
-cout<<"new added"<<endl;
-
-return 0;
-}
--- main3.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-
-cout<<"maaaaain3"<<endl;
-return 0;
-}
